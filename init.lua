6a27c1a9da89af39ef0cb0892b02d103c3ed0215
jumailj <jumailj.mail@gmail.com>
2024-04-14 20:40:33 +0530

    inital nvim config

---
+++ init.lua
@@ -0,0 +1,54 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader= " "
+
+vim.wo.relativenumber=true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins={
+  -- catppuccin Theme 
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- telescope fuzzy finder
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' } },
+  -- treesitter, indent
+  { "nvim-treesitter/nvim-treesitter", build=":TSUpdate"}
+}
+
+local opts={}
+
+require("lazy").setup(plugins, opts)
+
+-- TELESCOPE 
+local builtin = require("telescope.builtin")
+vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>Telescope find_files<cr>', {noremap = true, silent = true})
+-- alsway instal ripgrep, sudo apt-get install ripgrep
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+-- TREE SITTER
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript"},
+  highlight = {enable=true},
+  indent = {enable=true},
+})
+
+-- catppuccin THEME
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+
